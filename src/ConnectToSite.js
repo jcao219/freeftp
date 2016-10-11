323e84776797acbd7925be9621eaa41188583a43
Jimmy Cao <jc2462@cornell.edu>
2016-10-10 21:50:04 -0400

    Create a nice looking Connect To Site component

---
+++ src/ConnectToSite.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import {
+  Step,
+  Stepper,
+  StepLabel,
+} from 'material-ui/Stepper';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import './ConnectToSite.css';
+
+/**
+  * From http://www.material-ui.com/#/components/stepper
+  */
+export default class ConnectToSite extends React.Component {
+  state = {
+    finished: false,
+    stepIndex: 0,
+  };
+
+  handleNext = () => {
+    const {stepIndex} = this.state;
+    this.setState({
+      stepIndex: stepIndex + 1,
+      finished: stepIndex >= 2,
+    });
+  };
+
+  handlePrev = () => {
+    const {stepIndex} = this.state;
+    if (stepIndex > 0) {
+      this.setState({stepIndex: stepIndex - 1});
+    }
+  };
+
+  getStepContent(stepIndex) {
+    switch (stepIndex) {
+      case 0:
+        return 'TODO';
+      case 1:
+        return 'TODO';
+      case 2:
+        return 'TODO';
+      default:
+        return 'You\'re a long way from home sonny jim!';
+    }
+  }
+
+  render() {
+    const {finished, stepIndex} = this.state;
+    const contentStyle = {margin: '0 16px'};
+
+    return (
+      <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
+        <Stepper activeStep={stepIndex}>
+          <Step>
+            <StepLabel className={stepIndex === 0 ? "blackStepLabelBold" : "blackStepLabel"}>Connect to Site</StepLabel>
+      </Step>
+      <Step>
+        <StepLabel className={stepIndex === 1 ? "blackStepLabelBold" : "blackStepLabel"}>Save Site</StepLabel>
+      </Step>
+      <Step>
+        <StepLabel className={stepIndex === 2 ? "blackStepLabelBold" : "blackStepLabel"}>Confirm</StepLabel>
+      </Step>
+      </Stepper>
+      <div style={contentStyle}>
+        {finished ? (
+          <p>
+            <a
+              href="#"
+              onClick={(event) => {
+                event.preventDefault();
+                this.setState({stepIndex: 0, finished: false});
+              }}
+            >
+              Click here
+            </a> to reset the example.
+          </p>
+        ) : (
+          <div>
+            <p>{this.getStepContent(stepIndex)}</p>
+            <div style={{marginTop: 33}}>
+              <FlatButton
+                label="Back"
+                disabled={stepIndex === 0}
+                onTouchTap={this.handlePrev}
+                style={{marginRight: 12}}
+              />
+            <RaisedButton
+              label={stepIndex === 2 ? 'Finish' : 'Next'}
+              primary={true}
+              onTouchTap={this.handleNext}
+            />
+            </div>
+          </div>
+        )}
+      </div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import FtpClient from './ftp-client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ConnectToSite from './ConnectToSite';
+//import FtpClient from './ftp-client';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+/*
 document.addEventListener("DOMContentLoaded", function(event) { 
 
   document.getElementById('serverStart').addEventListener('click', function() {
@@ -17,3 +29,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     ftpClient.on('logged in', () => ftpClient.getPwd());
   });
 });
+*/
+
+ReactDOM.render(
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <ConnectToSite />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);

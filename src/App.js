1938a31c0cff38b1d5eb85a513b8653e6f4aaedd
Jimmy Cao <jc2462@cornell.edu>
2016-10-11 01:58:12 -0400

    Cool left navigation bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import TopBar from './TopBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle() {
+    this.setState({open: !this.state.open});
+  }
+
   render() {
-    return <TopBar />
+    return (<div><TopBar onDrawerToggle={this.handleToggle.bind(this)} />
+      <Drawer open={this.state.open} docked={false} 
+      onRequestChange={(open) => this.setState({open})}>
+        <MenuItem>Menu Item</MenuItem>
+      </Drawer></div>
+    )
   }
 }
--- src/TopBar.js
+++ src/TopBar.js
@@ -5,28 +5,32 @@ import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import NavigationFullscreen from 'material-ui/svg-icons/navigation/fullscreen';
 import NavigationChevronRight from 'material-ui/svg-icons/navigation/chevron-right';
 
-const TopBar = () => (
-  <AppBar
-    title={<span>Free FTP</span>}
-    style={{"-webkit-app-region": "drag"}}
-    iconElementLeft={
-      <IconButton
-        style={{"-webkit-app-region": "no-drag"}}>
-        <NavigationChevronRight />
-      </IconButton>
-    }
-    iconElementRight={(<div>
-      <IconButton
-        style={{"-webkit-app-region": "no-drag"}}>
-        <NavigationFullscreen />
-      </IconButton>
-      <IconButton
-        style={{"-webkit-app-region": "no-drag"}}
-        onClick={window.close.bind(window)}>
-        <NavigationClose />
-      </IconButton></div>
-    )}
-  />
-);
+export default class TopBar extends React.Component {
+  render() {
+    return (<AppBar
+      title={<span>Free FTP</span>}
+      style={{"-webkit-app-region": "drag"}}
+      iconElementLeft={
+        <IconButton
+          style={{"-webkit-app-region": "no-drag"}}
+          onClick={this.props.onDrawerToggle}>
+          <NavigationChevronRight />
+        </IconButton>
+      }
+      iconElementRight={(<div>
+        <IconButton
+          style={{"-webkit-app-region": "no-drag"}}
+          onClick={() => window.resizeTo(screen.availWidth, screen.availHeight)}>
+          <NavigationFullscreen />
+        </IconButton>
+        <IconButton
+          style={{"-webkit-app-region": "no-drag"}}
+          onClick={window.close.bind(window)}>
+          <NavigationClose />
+        </IconButton></div>
+      )}
+    />
+    )
+  }
+}
 
-export default TopBar;
